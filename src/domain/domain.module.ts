85b93a9ca247c05fa0b1cb48dd45efae45422627
Doroshkeviches <Doroshkeviches@yandex.ru>
2024-01-01 15:53:15 +0300

    confl

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -3,12 +3,13 @@ import { UsersRepoService } from './repos/user-repos.service';
 import { PrismaModule } from '../libs/prisma/src/prisma.module';
 import { RolesReposService } from './repos/roles-repos.service';
 import { DeviceRepoService } from './repos/device-repos.service';
+import { TicketsRepoService } from './repos/tickets-repos.service';
 import { CityReposService } from './repos/city-repos.service';
 import { FlightsRepoService } from './repos/flights-repos.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UsersRepoService, RolesReposService, CityReposService,DeviceRepoService,FlightsRepoService],
-  exports: [UsersRepoService, RolesReposService, CityReposService,DeviceRepoService,FlightsRepoService],
+  providers: [UsersRepoService, RolesReposService, CityReposService,DeviceRepoService, FlightsRepoService],
+  exports: [UsersRepoService, RolesReposService, CityReposService,DeviceRepoService, FlightsRepoService],
 })
 export class DomainModule {}
