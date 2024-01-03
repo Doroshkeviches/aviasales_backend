f7d8a25c96363b7dfe640d9bf9a99603fa854e0c
Doroshkeviches <Doroshkeviches@yandex.ru>
2024-01-03 09:56:51 +0300

    feat: add class-validator to auth module

--- src/app/auth/domain/ForgotPasswordForm.ts
+++ src/app/auth/domain/ForgotPasswordForm.ts
@@ -1,11 +1,18 @@
+import { EmailErrorMessage, UuidErrorMessage } from '@/src/libs/exceptions/i18n-error';
 import { IsEmail, IsUUID } from 'class-validator';
 import { validate } from 'class-validator';
 
 export class ForgotPasswordForm {
   @IsUUID()
+  @IsUUID(undefined, {
+    message: UuidErrorMessage,
+  })
   device_id: string;
 
   @IsEmail()
+  @IsEmail(undefined, {
+    message: EmailErrorMessage,
+  })
   email: string;
 
   static from(dto: ForgotPasswordForm) {
--- src/app/auth/domain/SignInForm.ts
+++ src/app/auth/domain/SignInForm.ts
@@ -1,26 +1,29 @@
+import { EmailErrorMessage, StrongPasswordErrorMessage, UuidErrorMessage } from '@/src/libs/exceptions/i18n-error';
 import { IsEmail } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUUID, validate } from 'class-validator';
+import { IsNotEmpty, IsString, IsStrongPassword, IsUUID, validate } from 'class-validator';
 
 export class SignInForm {
     @ApiProperty({
         description: 'email',
     })
     @IsEmail()
+    @IsEmail(undefined, {
+      message: EmailErrorMessage,
+    })
     email: string;
 
     @ApiProperty({
         description: 'password',
     })
-    @IsString()
-    @IsNotEmpty()
+    @IsStrongPassword(undefined, { message: StrongPasswordErrorMessage })
     password: string;
 
     @ApiProperty({
       description: 'device id',
     })
     @IsUUID(undefined, {
-      // message: UuidErrorMessage,
+      message: UuidErrorMessage,
     })
     device_id!: string;
 
