aa6e2a32ce08c2e7a102ff7b10957b6142d2e096
sevichok <90391121+sevichok@users.noreply.github.com>
2024-01-04 13:45:15 +0300

    flight controller formatter

--- src/app/flights/flights.controller.ts
+++ src/app/flights/flights.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { FlightsService } from './flights.service';
 import { ChangeFlightStatus } from './domain/ChangeFlightStatusForm';
 import { ErrorCodes } from '@/src/enums/error-codes.enum';
@@ -11,50 +19,65 @@ import { UserPermissions } from '@prisma/client';
 
 @Controller('flights')
 export class FlightsController {
-    constructor(private flightService: FlightsService) { }
-    @UseGuards(JwtAuthGuard)
-    @RequirePermissions(UserPermissions.GetArrayOfPath)
-    @Get()
-    async getArrayOfPath(@Query('from_city') from_city: string, @Query('to_city') to_city: string, @Query('date') date_string: string) {
-        const start_flight_date = new Date(date_string)
-        const from_city_entity = await this.flightService.getCityByTitle({ title: from_city })
-        const to_city_entity = await this.flightService.getCityByTitle({ title: to_city })
-        if (!from_city_entity || !to_city_entity) {
-            throw new ApiException(ErrorCodes.NoCity)
-        }
-        const flights = await this.flightService.getAllFlights({
-            start_flight_date,
-            from_city_id: from_city_entity.id
-        })
-        if (!flights) {
-            throw new ApiException(ErrorCodes.NoFlights)
-        }
-        const graph = await this.flightService.convertToGraph(flights)
-        const path = await this.flightService.findAllPaths(graph, from_city_entity, to_city_entity,{start_flight_date})
-        if (!path.length) {
-            throw new ApiException(ErrorCodes.NoPath)
-        }
-        const sortedPathByPrice = this.flightService.sortArraysByTotalPrice(path)
-        const sortedPathByTime = this.flightService.sortArraysByTotalTime(path)
-        return sortedPathByPrice
+  constructor(private flightService: FlightsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @RequirePermissions(UserPermissions.GetArrayOfPath)
+  @Get()
+  async getArrayOfPath(
+    @Query('from_city') from_city: string,
+    @Query('to_city') to_city: string,
+    @Query('date') date_string: string
+  ) {
+    const start_flight_date = new Date(date_string);
+    const from_city_entity = await this.flightService.getCityByTitle({
+      title: from_city,
+    });
+    const to_city_entity = await this.flightService.getCityByTitle({
+      title: to_city,
+    });
+    if (!from_city_entity || !to_city_entity) {
+      throw new ApiException(ErrorCodes.NoCity);
     }
-    @UseGuards(JwtAuthGuard)
-    @RequirePermissions(UserPermissions.ChangeFlightStatus)
-    @Post('status')
-    async changeFlightStatus(@Body() body: ChangeFlightStatus) {
-        const form = ChangeFlightStatus.from(body)
-        const errors = ChangeFlightStatus.validate(form)
-        if (errors) throw new ApiRequestException(ErrorCodes.InvalidForm, errors)
-        return this.flightService.changeFlightStatus(form)
+    const flights = await this.flightService.getAllFlights({
+      start_flight_date,
+      from_city_id: from_city_entity.id,
+    });
+    if (!flights) {
+      throw new ApiException(ErrorCodes.NoFlights);
     }
-    @UseGuards(JwtAuthGuard)
-    @RequirePermissions(UserPermissions.ChangeFlightPrice)
-   
-    @Post('price')
-    async changeFlightPrice(@Body() body: ChangeFlightPrice) {
-        const form = ChangeFlightPrice.from(body)
-        const errors = ChangeFlightPrice.validate(form)
-        if (errors) throw new ApiRequestException(ErrorCodes.InvalidForm, errors)
-        return this.flightService.changeFlightPrice(form)
+    const graph = await this.flightService.convertToGraph(flights);
+    const path = await this.flightService.findAllPaths(
+      graph,
+      from_city_entity,
+      to_city_entity,
+      { start_flight_date }
+    );
+    if (!path.length) {
+      throw new ApiException(ErrorCodes.NoPath);
     }
+    const sortedPathByPrice = this.flightService.sortArraysByTotalPrice(path);
+    const sortedPathByTime = this.flightService.sortArraysByTotalTime(path);
+    return sortedPathByPrice;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @RequirePermissions(UserPermissions.ChangeFlightStatus)
+  @Post('status')
+  async changeFlightStatus(@Body() body: ChangeFlightStatus) {
+    const form = ChangeFlightStatus.from(body);
+    const errors = ChangeFlightStatus.validate(form);
+    if (errors) throw new ApiRequestException(ErrorCodes.InvalidForm, errors);
+    return this.flightService.changeFlightStatus(form);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @RequirePermissions(UserPermissions.ChangeFlightPrice)
+  @Post('price')
+  async changeFlightPrice(@Body() body: ChangeFlightPrice) {
+    const form = ChangeFlightPrice.from(body);
+    const errors = ChangeFlightPrice.validate(form);
+    if (errors) throw new ApiRequestException(ErrorCodes.InvalidForm, errors);
+    return this.flightService.changeFlightPrice(form);
+  }
 }
