9928588d0bab9009fc2687ec6873fb365dddfa53
dxarch <emilepatch@gmail.com>
2024-01-08 14:21:46 +0300

    fix: merge conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module, MiddlewareConsumer, RequestMethod } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthModule } from "./app/auth/auth.module";
 import { CityModule } from "./app/city/city.module";
+import { FlightsModule } from "./app/flights/flights.module";
+import { ChatModule } from "@/src/app/chat/chat.module";
+import { RedisModule } from "@/src/app/redis/redis.module";
+import { SecurityModule } from "@/src/libs/security/src";
+import { UserModule } from "./app/user/user.module";
+import { TicketModule } from "./app/ticket/ticket.module";
 
 import config_app from "./config/app.config";
 import config_i18n from "./config/i18n.config";
@@ -9,15 +15,6 @@ import config_security from "./config/security.config";
 import { AcceptLanguageResolver, I18nModule, QueryResolver } from "nestjs-i18n";
 import { APP_FILTER } from "@nestjs/core";
 import { PrismaClientExceptionFilter } from "./libs/exceptions/global-exception.filter";
-import { FlightsModule } from "./app/flights/flights.module";
-
-import { ChatController } from "./app/chat/chat.controller";
-import { ChatModule } from "@/src/app/chat/chat.module";
-import { RedisModule } from "@/src/app/redis/redis.module";
-import { SecurityModule } from "@/src/libs/security/src";
-
-import { UserModule } from "./app/user/user.module";
-import { TicketModule } from "./app/ticket/ticket.module";
 
 @Module({
   imports: [
